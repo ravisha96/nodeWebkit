12101495e38004d45dea159cf80238fbfb31b7b6
ravisha96 <ravisha96@gmail.com>
2015-03-22 12:19:11 +0530

    login
    configure frontend and backend for request and response from node
    server and angular

---
+++ app/public/js/login/controller.js
@@ -0,0 +1,22 @@
+nw.controller('LoginCtrl', ['$scope', 'LoginService', '$http',
+
+	 function ($scope, LoginService, $http) {
+		
+		$scope.username = "ravisha96@gmail.com";
+		$scope.password = "1234546";
+
+		$scope.loginUser = function () {
+			$http.post('/login', {
+				
+				username: $scope.username,
+				password: $scope.password
+
+			});
+
+			console.log($scope.username);
+			console.log($scope.password);
+		};
+
+	}
+
+]);
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,3 +2,8 @@
 exports.index = function(req, res){
   res.render('index');
 };
+
+exports.login = function (req, res) {
+	console.log(res);
+	console.log(req);
+}
